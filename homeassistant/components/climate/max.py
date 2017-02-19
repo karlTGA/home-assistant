f91058a16411a8aec3f215eba24c7d05a32caef5
Karl Wolffgang <karlwolffgang@googlemail.com>
2017-02-19 18:58:25 +0100

    Add max heating system protokoll. First two devices are the thermostat and the wallthermostat.

---
+++ homeassistant/components/climate/max.py
@@ -0,0 +1,285 @@
+import logging
+import queue
+import voluptuous as vol
+
+from homeassistant.components.climate import (ClimateDevice, STATE_AUTO, STATE_IDLE, STATE_HEAT, ATTR_MAX_TEMP, ATTR_MIN_TEMP)
+from homeassistant.const import (TEMP_CELSIUS, STATE_UNKNOWN, ATTR_TEMPERATURE)
+import homeassistant.helpers.config_validation as cv
+from homeassistant.loader import get_component
+
+DEPENDENCIES = ['max']
+
+CUBE_ID = 0x123456
+DOMAIN = 'max'
+
+STATE_BOOST = 'Boost'
+
+ATTR_CURRENT_TEMP = 'current_temperature'
+ATTR_NAME = 'device_name'
+ATTR_ID = 'device_id'
+ATTR_BATTERY_STATE = 'battery_low'
+ATTR_DSTSETTING = 'dstsetting'
+ATTR_ISLOCKED = 'islocked'
+ATTR_LANGATEWAY = 'langateway'
+ATTR_LASTUPDATED = 'lastupdated'
+ATTR_MODE = 'mode'
+ATTR_RFERROR = 'rferror'
+ATTR_SIGNALSTRENGTH = 'signalstrength'
+ATTR_VALVEPOSITION = 'valveposition'
+
+"""Max Device Types"""
+MAX_CUBE = "Cube"
+MAX_THERMOSTAT = "HeatingThermostat"
+MAX_THERMOSTAT_PLUS = "HeatingThermostatPlus"
+MAX_WALLTHERMOSTAT = "WallMountedThermostat"
+MAX_SHUTTERCONTACT = "ShutterContact"
+MAX_PUSHBUTTON = "PushButton"
+
+
+_LOGGER = logging.getLogger(__name__)
+
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    max = get_component('max').MAX
+    """Setup the Max thermostat platform."""
+    if max is None:
+        return
+
+
+    devices = []
+    for device in config['devices']:
+        if 'wallthermostat' in device:
+            devices.append(MaxWallthermostat(max.thread, device['id']))
+        elif 'thermostat' in device:
+            devices.append(MaxThermostat(max.thread, device['id']))
+        elif 'shuttercontact' in device:
+            pass
+
+    add_devices(devices)
+    max.devices.extend(devices)
+
+
+
+class MaxDevice(ClimateDevice):
+    """Representation of a Sensor."""
+
+    def __init__(self, message_thread, device_id):
+        """Initialize the thermostat."""
+        self._thread = message_thread
+        self._current_temperature = None
+        self._target_temperature = 21
+        self._min_temperature = 4.5
+        self._max_temperature = 30.5
+        self._name = 'max device'
+        self._id = device_id
+        self._type = None
+        self._battery_low = None
+        self._dstsetting = None
+        self._is_locked = None
+        self._langateway = None
+        self._last_updated = None
+        self._mode = 'auto'
+        self._rferror = None
+        self._signal_strength = None
+        self._operation_list = ['auto', 'manual', 'boost']
+        self.update()
+
+
+    @property
+    def name(self):
+        """Return the name of the max thermostat, if any."""
+        return self._name
+
+    @property
+    def min_temp(self):
+        """Return the minimum temperature."""
+        return self._min_temperature
+
+    def set_min_temp(self, value):
+        self._min_temperature = value
+
+    @property
+    def max_temp(self):
+        """Return the minimum temperature."""
+        return self._max_temperature
+
+    def set_max_temp(self, value):
+        self._max_temperature = value
+
+    @property
+    def temperature_unit(self):
+        """Return the unit of measurement that is used."""
+        return TEMP_CELSIUS
+
+
+    @property
+    def id(self):
+        """Return the unit of measurement that is used."""
+        return self._id
+
+    @property
+    def type(self):
+        """Return the device type."""
+        return self._type
+
+    @property
+    def operation_mode(self):
+        """Return current operation ie. heat, cool, idle."""
+        return self._mode
+
+    @property
+    def operation_list(self):
+        """Return the operation modes list."""
+        return self._operation_list
+
+    @property
+    def mode(self):
+        """Return current mode ie. home, away, sleep."""
+        return self._mode
+
+    @property
+    def device_state_attributes(self):
+        """Return the device specific state attributes."""
+        state = {
+            ATTR_CURRENT_TEMP: self._current_temperature,
+            ATTR_TEMPERATURE: self._target_temperature,
+            ATTR_NAME: self._name,
+            ATTR_ID: self._id,
+            ATTR_BATTERY_STATE: self._battery_low,
+            ATTR_DSTSETTING: self._dstsetting,
+            ATTR_ISLOCKED: self._is_locked,
+            ATTR_LANGATEWAY: self._langateway,
+            ATTR_LASTUPDATED: self._last_updated,
+            ATTR_MODE: self._mode,
+            ATTR_RFERROR: self._rferror,
+            ATTR_SIGNALSTRENGTH: self._signal_strength,
+            ATTR_MIN_TEMP: self._min_temperature,
+            ATTR_MAX_TEMP: self._max_temperature
+        }
+
+        return state
+
+    def set_temperature(self, **kwargs):
+        """Set new target temperature."""
+        from maxcul.messages import SetTemperatureMessage
+
+        temperature = kwargs.get(ATTR_TEMPERATURE)
+        if temperature is None:
+            return
+
+        msg = SetTemperatureMessage(0xB9, 0, CUBE_ID, self._id, 0)
+        payload = {
+            'desired_temperature': temperature,
+            'mode': self._mode,
+        }
+        self._thread.command_queue.put((msg, payload))
+        self._target_temperature = temperature
+
+    def set_operation_mode(self, operation_mode):
+        """Set HVAC mode (auto, manual, boost)."""
+        from maxcul.messages import SetTemperatureMessage
+
+        if operation_mode is None:
+            return
+
+        msg = SetTemperatureMessage(0xB9, 0, CUBE_ID, self._id, 0)
+        payload = {
+            'desired_temperature': self._target_temperature,
+            'mode': operation_mode,
+        }
+        self._thread.command_queue.put((msg, payload))
+        self._mode = operation_mode
+
+class MaxThermostat(MaxDevice):
+
+    def __init__(self, message_thread, device_id):
+        super().__init__(message_thread, device_id)
+        self._valve_position = None
+        self._type = MAX_THERMOSTAT
+        self._name = self._type + ' ' + str(self._id)
+
+
+
+    @property
+    def current_operation(self):
+        """Return current operation."""
+        if self._valve_position is None:
+            return STATE_UNKNOWN
+        elif self._mode == 'boost':
+            return STATE_BOOST
+        elif self._valve_position > 0:
+            return STATE_HEAT
+        else:
+            return STATE_IDLE
+
+    @property
+    def device_state_attributes(self):
+        """Return the device specific state attributes."""
+        state = super().device_state_attributes
+        state.update({
+            ATTR_VALVEPOSITION: self._valve_position
+        })
+        return state
+
+    @property
+    def target_temperature(self):
+        """Return the temperature we try to reach."""
+        return self._target_temperature
+
+    def update(self):
+        """Get the latest data and updates the state."""
+
+        if self._thread.thermostat_states:
+            default = None
+            self._target_temperature = self._thread.thermostat_states[self._id].get('desired_temperature', default)
+            self._current_temperature = self._thread.thermostat_states[self._id].get('measured_temperature', default)
+            self._battery_low = self._thread.thermostat_states[self._id].get('battery_low', default)
+            self._dstsetting = self._thread.thermostat_states[self._id].get('dstsetting', default)
+            self._is_locked = self._thread.thermostat_states[self._id].get('is_locked', default)
+            self._langateway = self._thread.thermostat_states[self._id].get('langateway', default)
+            self._last_updated = self._thread.thermostat_states[self._id].get('last_updated', default)
+            self._mode = self._thread.thermostat_states[self._id].get('mode', default)
+            self._rferror = self._thread.thermostat_states[self._id].get('rferror', default)
+            self._signal_strength = self._thread.thermostat_states[self._id].get('signal_strenth', default)
+            self._valve_position = self._thread.thermostat_states[self._id].get('valve_position', default)
+
+class MaxWallthermostat(MaxDevice):
+
+    def __init__(self, message_thread, device_id):
+        super().__init__(message_thread, device_id)
+        self._until = None
+        self._display_actual_temprature = None
+        self._type = MAX_WALLTHERMOSTAT
+        self._name = self._type + ' ' + str(self._id)
+
+    @property
+    def target_temperature(self):
+        """Return the temperature we try to reach."""
+        return self._target_temperature
+
+    @property
+    def current_temperature(self):
+        """Return the current temperature."""
+        return self._current_temperature
+
+    @property
+    def current_operation(self):
+        """Return current operation."""
+        return self._mode
+
+    def update(self):
+        """Get the latest data and updates the state."""
+
+        if self._thread.wallthermostat_states:
+            default = None
+            self._target_temperature = self._thread.wallthermostat_states[self._id].get('desired_temperature', default)
+            self._current_temperature = self._thread.wallthermostat_states[self._id].get('temperature', default)
+            self._mode = self._thread.wallthermostat_states[self._id].get('mode', 'auto')
+            self._signal_strength = self._thread.wallthermostat_states[self._id].get('signal_strenth', default)
+            self._last_updated = self._thread.wallthermostat_states[self._id].get('last_updated', default)
+            self._battery_low = self._thread.wallthermostat_states[self._id].get('battery_low', default)
+            self._display_actual_temprature = self._thread.wallthermostat_states[self._id].get('display_actual_temperature', default)
+            self._dstsetting = self._thread.wallthermostat_states[self._id].get('dstsetting', default)
+            self._is_locked = self._thread.wallthermostat_states[self._id].get('is_locked', default)
+            self._langateway = self._thread.wallthermostat_states[self._id].get('langateway', default)
+            self._rferror = self._thread.wallthermostat_states[self._id].get('rferror', default)
+            self._until = self._thread.wallthermostat_states[self._id].get('until_str', default)
---
+++ homeassistant/components/max.py
@@ -0,0 +1,193 @@
+import logging
+import queue
+import voluptuous as vol
+from os import path
+
+import homeassistant.helpers.config_validation as cv
+from homeassistant.const import (ATTR_ENTITY_ID)
+from homeassistant.helpers.entity import Entity
+from homeassistant.config import load_yaml_config_file
+
+
+#REQUIREMENTS = ['maxcul==0.1.1']
+REQUIREMENTS = ['https://github.com/karlTGA/MaxCul-Python/archive/develop.zip#maxcul==0.1.2']
+
+CUBE_ID = 0x123456
+MAX = None
+DOMAIN = 'max'
+
+CONF_CULPATH = 'culpath'
+CONF_BAUDRATE = 'baudrate'
+
+DEFAULT_BAUDRATE = 38400
+DEFAULT_CULPATH = "/dev/ttyUSB0"
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Optional(CONF_BAUDRATE, default=DEFAULT_BAUDRATE): cv.positive_int,
+        vol.Required(CONF_CULPATH, default=DEFAULT_CULPATH): cv.string
+    })
+}, extra=vol.ALLOW_EXTRA)
+
+"""Const for set assocition service"""
+ATTR_TARGET_ID = 'target_id'
+ATTR_TARGET_TYPE = 'target_type'
+
+SERVICE_SET_MAX_ASSOCIATION = 'set_association'
+SET_MAX_ASSOCIATION_SCHEMA = vol.Schema({
+    vol.Required(ATTR_ENTITY_ID): cv.entity_id,
+    vol.Required(ATTR_TARGET_ID): cv.entity_id,
+})
+
+"""Const for set_config_temperature service"""
+ATTR_COMFORT_TEMPERATURE = 'comfort_temperature'
+ATTR_ECO_TEMPERATURE = 'eco_temperature'
+ATTR_MAX_TEMPERATURE = 'max_temperature'
+ATTR_MIN_TEMPERATURE = 'min_temperature'
+ATTR_MEASUREMENT_OFFSET = 'measurement_offset'
+ATTR_WINDOW_OPEN_TEMPERATURE = 'window_open_temperature'
+ATTR_WINDOW_OPEN_DURATION = 'window_open_duration'
+
+SERVICE_SET_MAX_CONFIG_TEMPERATURE = 'set_config_temperature'
+SET_MAX_CONFIG_TEMPERATURE_SCHEMA = vol.Schema({
+    vol.Required(ATTR_ENTITY_ID): cv.entity_id,
+    vol.Required(ATTR_COMFORT_TEMPERATURE):vol.Coerce(float),
+    vol.Required(ATTR_ECO_TEMPERATURE): vol.Coerce(float),
+    vol.Required(ATTR_MAX_TEMPERATURE): vol.Coerce(float),
+    vol.Required(ATTR_MIN_TEMPERATURE): vol.Coerce(float),
+    vol.Required(ATTR_MEASUREMENT_OFFSET): vol.Coerce(float),
+    vol.Required(ATTR_WINDOW_OPEN_TEMPERATURE): vol.Coerce(float),
+    vol.Required(ATTR_WINDOW_OPEN_DURATION): cv.positive_int
+})
+
+
+def setup(hass, config):
+
+    conf = config[DOMAIN]
+    culpath = conf.get(CONF_CULPATH)
+    baudrate = conf.get(CONF_BAUDRATE)
+
+    descriptions = load_yaml_config_file(
+        path.join(path.dirname(__file__), 'services.yaml')).get(DOMAIN)
+
+    """Define the set_association service"""
+    def set_association(service):
+        """associate the thermostat with a other device"""
+        from maxcul.messages import AddLinkPartnerMessage
+
+        entity_id = service.data.get(ATTR_ENTITY_ID)
+        target_id = service.data.get(ATTR_TARGET_ID)
+
+        dev1 = None
+        dev2 = None
+        for dev in MAX.devices:
+            if dev.entity_id == entity_id:
+                dev1 = dev
+            elif dev.entity_id == target_id:
+                dev2 = dev
+
+        if dev1 is None or dev2 is None:
+            return
+
+        msg = AddLinkPartnerMessage(0xB9, 0, CUBE_ID, dev1.id, 0)
+        msg.counter = 0xB9
+        msg.sender_id = CUBE_ID
+        msg.receiver_id = dev1.id
+        msg.group_id = 0
+        payload = {
+            'assocDevice': dev2.id,
+            'assocDeviceType': dev2.type
+        }
+        MAX.thread.command_queue.put((msg, payload))
+
+        msg = AddLinkPartnerMessage(0xB9, 0, CUBE_ID, dev2.id, 0)
+        payload = {
+            'assocDevice': dev1.id,
+            'assocDeviceType': dev1.type
+        }
+        MAX.thread.command_queue.put((msg, payload))
+
+    hass.services.register(DOMAIN, SERVICE_SET_MAX_ASSOCIATION, set_association, descriptions.get(SERVICE_SET_MAX_ASSOCIATION), schema=SET_MAX_ASSOCIATION_SCHEMA)
+
+    """Define the set_config_temperature service"""
+    def set_config_temperature(service):
+        """associate the thermostat with a other device"""
+        from maxcul.messages import ConfigTemperaturesMessage
+
+        entity_id = service.data.get(ATTR_ENTITY_ID)
+        comfort_temperature = service.data.get(ATTR_COMFORT_TEMPERATURE)
+        eco_temperature = service.data.get(ATTR_ECO_TEMPERATURE)
+        max_temperature = service.data.get(ATTR_MAX_TEMPERATURE)
+        min_temperature = service.data.get(ATTR_MIN_TEMPERATURE)
+        measurement_offset = service.data.get(ATTR_MEASUREMENT_OFFSET)
+        window_open_temperature = service.data.get(ATTR_WINDOW_OPEN_TEMPERATURE)
+        window_open_duration = service.data.get(ATTR_WINDOW_OPEN_DURATION)
+
+        entity = None
+        for dev in MAX.devices:
+            if dev.entity_id == entity_id:
+                entity = dev
+
+        if entity is None or entity.type in ["Cube", "ShutterContact", "PushButton"]:
+            return
+
+        msg = ConfigTemperaturesMessage(0xB9, 0, CUBE_ID, entity.id, 0)
+        payload = {
+            'comfort_Temperature': comfort_temperature,
+            'eco_Temperature': eco_temperature,
+            'max_Temperature': max_temperature,
+            'min_Temperature': min_temperature,
+            'measurement_Offset': measurement_offset,
+            'window_Open_Temperatur': window_open_temperature,
+            'window_Open_Duration': window_open_duration
+        }
+        MAX.thread.command_queue.put((msg, payload))
+        entity.set_max_temp(max_temperature)
+        entity.set_min_temp(min_temperature)
+
+    hass.services.register(DOMAIN, SERVICE_SET_MAX_CONFIG_TEMPERATURE, set_config_temperature, descriptions.get(SERVICE_SET_MAX_CONFIG_TEMPERATURE), schema=SET_MAX_CONFIG_TEMPERATURE_SCHEMA)
+
+
+    global MAX
+    if MAX is None:
+        MAX = MaxCul(culpath, baudrate)
+        return True
+    else:
+        return False
+
+class MaxCul(Entity):
+    def __init__(self, culpath, baudrate):
+        """Setup the Max cul platform."""
+        from maxcul.communication import CULMessageThread
+        from logbook.more import ColorizedStderrHandler
+        self._log_handler = ColorizedStderrHandler()
+        self._log_handler.push_application()
+        self._name = 'Max CUL Stick'
+        self._devices = []
+        self._queue = queue.Queue()
+        self._thread = CULMessageThread(self._queue, culpath, baudrate)
+        self._thread.start()
+        # message_thread.join()
+
+    @property
+    def name(self):
+        """Return the name of the max thermostat, if any."""
+        return self._name
+
+    @property
+    def thread(self):
+        """Return the unit of measurement that is used."""
+        return self._thread
+
+    @property
+    def queue(self):
+        """Return the unit of measurement that is used."""
+        return self._queue
+
+    @property
+    def devices(self):
+        """Return the unit of measurement that is used."""
+        return self._devices
+
+
+
